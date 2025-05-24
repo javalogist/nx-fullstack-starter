f99b413e764b37fad3d6987e09de1abc3aade0d3
javalogist <developer.javalogist@gmail.com>
2025-05-24 21:10:28 +0530

    Update: fix minor issues, unused files removal and update next app name

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../libs/client/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./web/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../libs/frontend/src/lib/ui/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
     container: {
